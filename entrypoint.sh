a4e6ab0bb3074d5eb166b299ce9a44fe0bf1b46c
vikas.nautiyal <vikas.nautiyal@stashfin.com>
2024-10-12 13:57:30 +0530

    Deployment Strategy

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+echo "Starting the first batch of scripts..."
+
+# First Batch of Scripts
+node uscripts/installment-fip.js
+node uscripts/installment-pif.js
+node uscripts/overall-payment.js
+node uscripts/vpenalty-details-pif.js
+node uscripts/penalty-details.js
+node uscripts/st-bill-details.js
+node uscripts/st-loan-update.js
+
+echo "First batch completed. Starting the second batch of scripts..."
+
+# Second Batch of Scripts
+node uscripts/installment-payment-fip.js
+node uscripts/installment-payment-pif.js
+
+echo "All scripts have been executed successfully."
